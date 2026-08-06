9d3b0d28ee2ba958c6a4dd20a4365b4750f11099
Swomp <currentswomp@gmail.com>
2026-08-06 11:05:38 +0300

    Добавлен flake.nix для установки нужных пакетов на nixos

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Проект сайта первичного отделения района Печатники";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      desShells."${system}".default = pkgs.mkShell {
+        packages = with pkgs; [
+          uv
+          python313
+        ];
+
+        shellHook = ''
+          echo "Python: $(${pkgs.python313}/bin/python3 --version)"
+          echo "UV: $(${pkgs.uv}/bin/uv --version)"
+
+          if [[ -z "''${FISH_VERSION:-}" && -t 1 ]]; then
+            exec ${pkgs.fish}/bin/fish
+          fi
+        '';
+      };
+    };
+}
